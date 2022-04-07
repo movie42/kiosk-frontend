dce6b819786d9e5c37e6d8c4464fb5a50bb19f44
ko Hyunsu <movie3342@gmail.com>
2022-04-07 09:39:14 +0900

    feat: productList 목업 변경 및 productItemState 추가
    mockup/productList.ts
        - interface ProductListValue에 select:boolean 타입 추가
        - productList 리스트에 select 추가했을
    
    productItemState.ts
        - productList를 전역 상태로 관리하기 위해 recoil atom 추가

--- project/client/src/mockup/productList.ts
+++ project/client/src/mockup/productList.ts
@@ -1,8 +1,9 @@
-interface ProductListValues {
-  id: number | string;
+export interface ProductListValues {
+  id: number;
   thumbnail: string | null | undefined;
   name: string;
   price: number;
+  select?: boolean;
 }
 
 export const productList: ProductListValues[] = [
@@ -11,47 +12,55 @@ export const productList: ProductListValues[] = [
     thumbnail: null,
     name: "소고기 덮밥",
     price: 9000,
+    select: false,
   },
   {
     id: 2,
     thumbnail: null,
     name: "돼지고기 덮밥",
     price: 8000,
+    select: false,
   },
   {
     id: 3,
     thumbnail: null,
     name: "장어 덮밥",
     price: 11000,
+    select: false,
   },
   {
     id: 4,
     thumbnail: null,
     name: "가지 덮밥",
     price: 7000,
+    select: false,
   },
   {
     id: 5,
     thumbnail: null,
     name: "채소 튀김 모듬 덮밥",
     price: 8000,
+    select: false,
   },
   {
     id: 6,
     thumbnail: null,
     name: "돈부리",
     price: 8000,
+    select: false,
   },
   {
     id: 7,
     thumbnail: null,
     name: "라면 덮밥",
     price: 5000,
+    select: false,
   },
   {
     id: 8,
     thumbnail: null,
     name: "카레 덮밥",
     price: 7000,
+    select: false,
   },
 ];
