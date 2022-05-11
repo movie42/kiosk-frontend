1f51e2403d58475d57411b97a597036fea9d789e
movie42 <movie3342@gmail.com>
2022-05-11 14:07:27 +0900

    feat:status바에서 뒤로가기 버튼 추가

--- project/src/Page/Admin/AdminManageProductAddItem.tsx
+++ project/src/Page/Admin/AdminManageProductAddItem.tsx
@@ -309,7 +309,7 @@ const AdminManageProductAddItem = () => {
         <ButtonContainer>
           <h3>상품 입력이 끝나면 등록하기 버튼을 눌러주세요.</h3>
           <div>
-            <CancelButton>등록 취소</CancelButton>
+            <CancelButton onClick={() => navigate(-1)}>등록 취소</CancelButton>
             <CreateButton onClick={onSubmit}>상품 등록</CreateButton>
           </div>
         </ButtonContainer>
