b9c8aceabf9326a19ce686ad71c784a6342ce1e7
ko Hyunsu <movie3342@gmail.com>
2022-05-17 13:40:32 +0900

    fix:orderList mockup 데이터 형태 변경
    데이터 형태를 주문 번호를 기준으로 배열로 모으는 형태로 변경

--- src/mockup/orderList.ts
+++ src/mockup/orderList.ts
@@ -1,6 +1,11 @@
 export interface Order {
   id: number;
   orderNumber: number;
+  orderList: OrderList[];
+}
+
+export interface OrderList {
+  productId: number;
   productName: string;
   quantity: number;
   price: number;
@@ -12,91 +17,125 @@ export const orderList: Order[] = [
   {
     id: 1,
     orderNumber: 1001,
-    productName: "소고기 덮밥",
-    quantity: 1,
-    price: 8000,
-    optionID: "기본",
-    state: "complete",
+    orderList: [
+      {
+        productId: 1,
+        productName: "소고기 덮밥",
+        quantity: 1,
+        price: 8000,
+        optionID: "기본",
+        state: "complete",
+      },
+      {
+        productId: 1,
+        productName: "소고기 덮밥",
+        quantity: 1,
+        price: 8000,
+        optionID: "매운맛",
+        state: "complete",
+      },
+    ],
   },
   {
     id: 2,
-    orderNumber: 1001,
-    productName: "소고기 덮밥",
-    quantity: 1,
-    price: 8000,
-    optionID: "매운맛",
-    state: "complete",
-  },
-  {
-    id: 3,
     orderNumber: 1002,
-    productName: "장어 덮밥",
-    quantity: 3,
-    price: 30000,
-    optionID: "기본",
-    state: "order",
+    orderList: [
+      {
+        productId: 2,
+        productName: "장어 덮밥",
+        quantity: 3,
+        price: 30000,
+        optionID: "기본",
+        state: "order",
+      },
+    ],
   },
   {
     id: 4,
-    orderNumber: 1005,
-    productName: "돼지고기 덮밥",
-    quantity: 2,
-    price: 8000,
-    optionID: "기본",
-    state: "order",
+    orderNumber: 1003,
+    orderList: [
+      {
+        productId: 3,
+        productName: "돼지고기 덮밥",
+        quantity: 2,
+        price: 8000,
+        optionID: "기본",
+        state: "order",
+      },
+      {
+        productId: 1,
+        productName: "소고기 덮밥",
+        quantity: 1,
+        price: 8000,
+        optionID: "매운맛",
+        state: "order",
+      },
+      {
+        productId: 1,
+        productName: "소고기 덮밥",
+        quantity: 2,
+        price: 16000,
+        optionID: "아주 매운맛",
+        state: "cancel",
+      },
+    ],
   },
-  {
-    id: 5,
-    orderNumber: 1005,
-    productName: "소고기 덮밥",
-    quantity: 1,
-    price: 8000,
-    optionID: "매운맛",
-    state: "order",
-  },
-  {
-    id: 6,
-    orderNumber: 1005,
-    productName: "소고기 덮밥",
-    quantity: 2,
-    price: 16000,
-    optionID: "아주 매운맛",
-    state: "cancel",
-  },
+
   {
     id: 7,
     orderNumber: 1005,
-    productName: "소고기 덮밥",
-    quantity: 2,
-    price: 16000,
-    optionID: "아주 매운맛",
-    state: "cancel",
+    orderList: [
+      {
+        productId: 1,
+        productName: "소고기 덮밥",
+        quantity: 2,
+        price: 16000,
+        optionID: "아주 매운맛",
+        state: "cancel",
+      },
+    ],
   },
   {
     id: 8,
-    orderNumber: 1005,
-    productName: "소고기 덮밥",
-    quantity: 2,
-    price: 16000,
-    optionID: "아주 매운맛",
-    state: "cancel",
+    orderNumber: 1004,
+    orderList: [
+      {
+        productId: 3,
+        productName: "돼지고기 덮밥",
+        quantity: 2,
+        price: 16000,
+        optionID: "아주 매운맛",
+        state: "cancel",
+      },
+    ],
   },
+
   {
     id: 9,
     orderNumber: 2001,
-    productName: "소고기 덮밥",
-    quantity: 1,
-    price: 8000,
-    optionID: "매운맛",
-    state: "cancel",
+    orderList: [
+      {
+        productId: 1,
+        productName: "소고기 덮밥",
+        quantity: 1,
+        price: 8000,
+        optionID: "매운맛",
+        state: "cancel",
+      },
+    ],
   },
   {
     id: 10,
     orderNumber: 2002,
-    productName: "소고기 덮밥",
-    quantity: 1,
-    price: 8000,
-    optionID: "기본",
-    state: "cancel",
+    orderList: [
+      {
+        productId: 1,
+        productName: "소고기 덮밥",
+        quantity: 1,
+        price: 8000,
+        optionID: "기본",
+        state: "cancel",
+      },
+    ],
   },
 ];
