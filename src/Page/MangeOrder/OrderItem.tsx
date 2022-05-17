77e16ec44bd9d7dc12d3a9452658111c3eb965b8
ko Hyunsu <movie3342@gmail.com>
2022-05-17 15:12:46 +0900

    feat:OrderStateList와 OrderItem 컴포넌트 설계
    OrderStateList
    - orderList를 주문 번호를 기준으로 OrderItem으로 넘김
    OrderItem
    - 넘어온 orderList를 받아 주문번호를 기준으로 완료 취소 주문 상품을 보여준다.
    - Array.prototype.some과 every를 사용해서 상태를 그려준다.
    - 문제는 orderList를 some으로 필터링을 하기 때문에 주문 중에 부분 취소가 있을 경우에 cancel로 sort된 아이템도 cancel sort에 포함된다.

---
+++ src/Page/MangeOrder/OrderItem.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { OrderList } from "../../mockup/orderList";
+import { CancelButton, CompleteButton, OrderButton } from "./OrderStateList";
+
+interface IOrderItemProp {
+  orderList: OrderList[];
+  handleCancelStateModal: () => any;
+  handleOrderStateModal: () => any;
+  handleCompleteStateModal: () => any;
+}
+const OrderItem = ({
+  orderList,
+  handleCancelStateModal,
+  handleOrderStateModal,
+  handleCompleteStateModal,
+}: IOrderItemProp) => {
+  return (
+    <>
+      {orderList.some((item) => item.state) && (
+        <>
+          <span>
+            {orderList[0].productName}{" "}
+            {orderList.length > 1 ? `외 ${orderList.length}` : ""}
+          </span>
+          <span>
+            {orderList.every((item) => item.state) && (
+              <>
+                <CancelButton
+                  onClick={handleCancelStateModal}
+                  state={orderList[0].state}
+                >
+                  취소
+                </CancelButton>
+                <OrderButton
+                  onClick={handleOrderStateModal}
+                  state={orderList[0].state}
+                >
+                  주문
+                </OrderButton>
+                <CompleteButton
+                  onClick={handleCompleteStateModal}
+                  state={orderList[0].state}
+                >
+                  완료
+                </CompleteButton>
+              </>
+            )}
+          </span>
+        </>
+      )}
+    </>
+  );
+};
+
+export default OrderItem;
--- src/Page/MangeOrder/OrderStateList.tsx
+++ src/Page/MangeOrder/OrderStateList.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import ButtonDefaultStyle from "../../Components/Buttons/ButtonDefault";
 import { SubTitle1, SubTitle2 } from "../../mixin";
-import { Order } from "../../mockup/orderList";
+import { Order, OrderList } from "../../mockup/orderList";
+import OrderItem from "./OrderItem";
 
 const List = styled.ul`
   li {
@@ -23,15 +24,15 @@ const List = styled.ul`
   }
 `;
 
-const OrderButton = styled(ButtonDefaultStyle)<IOrderState>`
+export const OrderButton = styled(ButtonDefaultStyle)<IOrderState>`
   background-color: ${(props) =>
     props.state === "order" && props.theme.color.secondary400};
 `;
-const CompleteButton = styled(ButtonDefaultStyle)<IOrderState>`
+export const CompleteButton = styled(ButtonDefaultStyle)<IOrderState>`
   background-color: ${(props) =>
     props.state === "complete" && props.theme.color.primary700};
 `;
-const CancelButton = styled(ButtonDefaultStyle)<IOrderState>`
+export const CancelButton = styled(ButtonDefaultStyle)<IOrderState>`
   background-color: ${(props) =>
     props.state === "cancel" && props.theme.color.error500};
 `;
@@ -41,10 +42,14 @@ interface IOrderProps {
 }
 
 interface IOrderState {
-  state: Order["state"];
+  state: OrderList["state"];
 }
 
 const OrderStateList = ({ orders }: IOrderProps) => {
+  const handleCancelStateModal = () => {};
+  const handleOrderStateModal = () => {};
+  const handleCompleteStateModal = () => {};
+
   return (
     <List>
       <li>
@@ -55,24 +60,12 @@ const OrderStateList = ({ orders }: IOrderProps) => {
       {orders.map((order) => (
         <li key={order.id} data-id={order.id}>
           <span>{order.orderNumber}</span>
-          <span>{order.productName}</span>
-          {order.state === "order" && (
-            <span>
-              <CancelButton state={order.state}>취소</CancelButton>
-              <OrderButton state={order.state}>주문</OrderButton>
-              <CompleteButton state={order.state}>완료</CompleteButton>
-            </span>
-          )}
-          {order.state === "complete" && (
-            <span>
-              <CompleteButton state={order.state}>완료</CompleteButton>
-            </span>
-          )}
-          {order.state === "cancel" && (
-            <span>
-              <CancelButton state={order.state}>취소</CancelButton>
-            </span>
-          )}
+          <OrderItem
+            orderList={order.orderList}
+            handleCancelStateModal={handleCancelStateModal}
+            handleOrderStateModal={handleOrderStateModal}
+            handleCompleteStateModal={handleCompleteStateModal}
+          />
         </li>
       ))}
     </List>
