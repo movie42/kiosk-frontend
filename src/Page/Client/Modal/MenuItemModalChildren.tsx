6935ba27987182a939c3c65921ff7397d08db837
sycho09 <wyswid@naver.com>
2022-05-30 20:28:38 +0900

    style: 메뉴 선택 모달, 주문 메뉴 리스트 스타일 변경

--- src/Page/Client/Modal/MenuItemModalChildren.tsx
+++ src/Page/Client/Modal/MenuItemModalChildren.tsx
@@ -1,12 +1,40 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+import { IoIosAddCircle } from "react-icons/io";
+import { AiFillMinusCircle } from "react-icons/ai";
 import { ProductListValues } from "../../../mockup/productList";
 import ButtonDefaultStyle from "../../../Components/Buttons/ButtonDefault";
-import styled from "styled-components";
 import { IOrderSelectedItem } from "../ClientMenu";
+import { AddCountButton, MinusCountButton } from "../ClientSelectList";
 
 const OptionButton = styled(ButtonDefaultStyle)<{ selected?: boolean }>`
   background-color: ${(props) =>
     props.selected ? props.theme.color.primary800 : props.theme.color.gray300};
+  margin-right: 1rem;
+`;
+const OrderContainer = styled.div`
+  display: grid;
+  grid-template-columns: 50px 0.5fr 50px 1fr 1fr;
+  column-gap: 20px;
+  align-items: center;
+`;
+const CancelButton = styled(ButtonDefaultStyle)`
+  background-color: ${(props) => props.theme.color.gray300};
+`;
+const OrderButton = styled(ButtonDefaultStyle)`
+  background-color: ${(props) => props.theme.color.primary800};
+  padding: 0.8rem 1rem;
+`;
+const Title = styled.h4`
+  font-size: 1.7rem;
+  font-weight: bold;
+  margin: 2rem 0 0.5rem;
+`;
+const ProductCount = styled.span`
+  font-size: 2rem;
+  font-weight: bold;
+  margin: 0 1.5rem 0.8rem;
+  text-align: center;
 `;
 
 interface IMenuItemModalChildrenProps {
@@ -97,8 +125,10 @@ const MenuItemModalChildren: React.FC<IMenuItemModalChildrenProps> = ({
   return (
     <>
       <h2>{selectedItem[0].name}</h2>
-      <p>주문 수: {count}</p>
-      <p>주문 가격: {count * Number(selectedItem[0].price)}</p>
+      <Title>구성</Title>
+      <p>{selectedItem[0].name} 1개</p>
+      <Title>상품옵션</Title>
+
       {selectedItem[0].option?.map((item, i) => (
         <OptionButton
           key={item}
@@ -109,24 +139,31 @@ const MenuItemModalChildren: React.FC<IMenuItemModalChildrenProps> = ({
         </OptionButton>
       ))}
 
-      <button
-        onClick={() => {
-          if (count < 1) return;
-          setCount(count - 1);
-        }}
-      >
-        -
-      </button>
-      <button
-        onClick={() => {
-          if (count < 0) return;
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
-      <button onClick={cancelItem}>취소하기</button>
-      <button onClick={orderSelectedItem}>주문하기</button>
+      <Title>
+        총 가격&nbsp;
+        {(count * Number(selectedItem[0].price)).toLocaleString()}원
+      </Title>
+      <OrderContainer>
+        <MinusCountButton
+          onClick={() => {
+            if (count < 1) return;
+            setCount(count - 1);
+          }}
+        >
+          <AiFillMinusCircle />
+        </MinusCountButton>
+        <ProductCount>{count}</ProductCount>
+        <AddCountButton
+          onClick={() => {
+            if (count < 0) return;
+            setCount(count + 1);
+          }}
+        >
+          <IoIosAddCircle />
+        </AddCountButton>
+        <CancelButton onClick={cancelItem}>취소하기</CancelButton>
+        <OrderButton onClick={orderSelectedItem}>주문하기</OrderButton>
+      </OrderContainer>
     </>
   );
 };
