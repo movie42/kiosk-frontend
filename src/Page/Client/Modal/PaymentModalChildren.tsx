9a194ec2aecd7ad9775327381a07594ffdef676d
sycho09 <wyswid@naver.com>
2022-05-31 16:13:38 +0900

    feat: 화면 자동전환 타이머 구현

--- src/Page/Client/Modal/PaymentModalChildren.tsx
+++ src/Page/Client/Modal/PaymentModalChildren.tsx
@@ -2,17 +2,54 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { useRecoilState } from "recoil";
+import { Headline2, SubTitle2, Body1 } from "../../../mixin";
 import { selectMenuListState } from "../../../state/productItemState";
+import ButtonDefaultStyle from "../../../Components/Buttons/ButtonDefault";
 
 interface IPaymentModalChildrenProps {
   setIsModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const PaymentBox = styled.div`
-  p {
-    font-size: 1.5rem;
-    padding: 0.3rem 0;
+  h1 {
+    text-align: center;
   }
+  h2 {
+    ${Headline2};
+  }
+  h3 {
+    ${SubTitle2};
+    font-weight: 700;
+  }
+  span {
+    ${Body1};
+  }
+  p {
+    ${Body1};
+    font-weight: 700;
+  }
+`;
+const MenuBox = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr 1fr;
+  padding-bottom: 0.5rem;
+  margin-bottom: 1rem;
+  border-bottom: 1px solid black;
+  text-align: right;
+  span:nth-child(3n-2) {
+    text-align: left;
+  }
+`;
+const BtnGroup = styled.div`
+  text-align: center;
+`;
+const ConfirmButton = styled(ButtonDefaultStyle)`
+  background-color: ${(props) => props.theme.color.primary500};
+  margin: 0 0.5rem;
+`;
+const CancelButton = styled(ButtonDefaultStyle)`
+  background-color: ${(props) => props.theme.color.gray300};
+  margin: 0 0.5rem;
 `;
 
 const PaymentModalChildren: React.FC<IPaymentModalChildrenProps> = ({
@@ -24,10 +61,15 @@ const PaymentModalChildren: React.FC<IPaymentModalChildrenProps> = ({
   const [isPaid, setIsPaid] = useState(false);
   // print done
   const [isPrint, setIsPrint] = useState(false);
-
   // handle print receipt
   const [printReceipt, setPrintReceipt] = useState<Boolean>(false);
+
+  // set time out
+  let showTimer: NodeJS.Timeout;
+  let closeTimer: NodeJS.Timeout;
   const handleReceipt = (receipt: Boolean) => {
+    clearInterval(showTimer);
+    clearTimeout(closeTimer);
     setPrintReceipt(receipt);
     setIsPrint(true);
   };
@@ -38,28 +80,77 @@ const PaymentModalChildren: React.FC<IPaymentModalChildrenProps> = ({
     setOrderList([]);
     navigate("/client/main");
   };
+
+  // close the receipt modal automatically in 5 secs
+  const [remain, setRemain] = useState(5);
+  const closeReceipt = () => {
+    setIsPaid(true);
+    setPrintReceipt(true);
+    showTimer = setInterval(() => {
+      setRemain((prv) => prv - 1);
+    }, 1000);
+
+    closeTimer = setTimeout(() => {
+      clearInterval(showTimer);
+      clearTimeout(closeTimer);
+      setIsPrint(true);
+    }, 5000);
+  };
+
+  console.log(orderList);
+
   return (
     <PaymentBox>
       {!isPaid && (
         <>
           <h2>결제중입니다</h2>
-          <p>카드를 삽입해주세요</p>
-          <button onClick={() => setIsPaid(true)}>결제완료</button>
+          <h3>카드를 삽입해주세요</h3>
+          <p>
+            총 결제&nbsp;
+            {orderList
+              .reduce(function (acc, obj) {
+                return acc + obj.totalPrice;
+              }, 0)
+              .toLocaleString()}
+            원
+          </p>
+          <p>주문 상품</p>
+          {orderList.map((el, i) => (
+            <MenuBox key={`${el.productId}${el.option}`}>
+              <span>
+                {el.name} {el.option}
+              </span>
+              <span>{el.totalCount}개</span>
+              <span>{el.totalPrice.toLocaleString()}원</span>
+            </MenuBox>
+          ))}
+          <button onClick={closeReceipt}>결제완료</button>
           <button onClick={() => setIsModal(false)}>취소하기</button>
         </>
       )}
-      {!isPaid && !isPrint ? (
+      {isPaid && !isPrint && (
         <>
           <h2>결제가 완료되었습니다</h2>
-          <p>영수증을 출력하시겠습니까?</p>
-          <button onClick={() => handleReceipt(true)}>예</button>
-          <button onClick={() => handleReceipt(false)}>아니요</button>
+          <h3>영수증을 출력하시겠습니까?</h3>
+          <h1>{remain}</h1>
+          <span>선택하지 않을 경우 자동으로 출력됩니다</span>
+          <BtnGroup>
+            <ConfirmButton onClick={() => handleReceipt(true)}>
+              예
+            </ConfirmButton>
+            <CancelButton onClick={() => handleReceipt(false)}>
+              아니요
+            </CancelButton>
+          </BtnGroup>
         </>
-      ) : (
+      )}
+      {isPaid && isPrint && (
         <>
           <h2>주문이 완료되었습니다</h2>
-          <p>주문 번호를 확인해주세요</p>
-          <button onClick={confirmOrder}>확인</button>
+          <h3>주문 번호를 확인해주세요</h3>
+          <BtnGroup>
+            <ConfirmButton onClick={confirmOrder}>확인</ConfirmButton>
+          </BtnGroup>
         </>
       )}
     </PaymentBox>
