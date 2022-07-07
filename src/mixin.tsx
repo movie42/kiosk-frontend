8572472675f6ee6d3d221c49ebf1818fa422b8ab
A <movie3342@gmail.com>
2022-07-07 18:59:01 +0900

    fix:mixin 추가 및 폰트 사이즈 변경
    Headline3와 Body2를 추가하였고 글자 크기를 변경하였습니다.

--- src/mixin.tsx
+++ src/mixin.tsx
@@ -1,34 +1,48 @@
 export const Headline1 = `
-  font-size: 5rem;
+  font-size: 5.5rem;
   font-weight: 900;
   line-height: 2;
   letter-spacing: -0.2;
 `;
 
 export const Headline2 = `
-  font-size: 3rem;
+  font-size: 4rem;
+  font-weight: 900;
+  line-height: 2;
+  letter-spacing: -0.2;
+`;
+
+export const Headline3 = `
+  font-size: 3.2rem;
   font-weight: 900;
   line-height: 2;
   letter-spacing: -0.2;
 `;
 
 export const SubTitle1 = `
-  font-size: 2.4rem;
+  font-size: 2.6rem;
   font-weight: 700;
   line-height: 1.7;
   letter-spacing: -0.1;
 `;
 
 export const SubTitle2 = `
-  font-size: 2rem;
+  font-size: 2.2rem;
   font-weight: 400;
   line-height: 1.7;
   letter-spacing: -0.1;
 `;
 
 export const Body1 = `
+  font-size: 1.8rem;
+  font-weight: normal;
+  line-height: 1.8;
+  letter-spacing: -0.1;
+`;
+
+export const Body2 = `
   font-size: 1.6rem;
-  font-weight: 400;
-  line-height: 1.5;
+  font-weight: normal;
+  line-height: 1.8;
   letter-spacing: -0.1;
 `;
