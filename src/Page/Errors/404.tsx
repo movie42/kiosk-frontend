98b7658e35699ffd7a51d2bca8cde1751ae89569
Chosy <wyswid@gmail.com>
2022-07-16 01:04:02 +0900

    style: Not found 돌아가기 버튼 추가

--- src/Page/Errors/404.tsx
+++ src/Page/Errors/404.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import ButtonDefaultStyle from "../../Components/Buttons/ButtonDefault";
+import { SubTitle2 } from "../../mixin";
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 1rem;
+  p {
+    ${SubTitle2}
+  }
+`;
+const ConfirmButton = styled(ButtonDefaultStyle)`
+  color: ${(props) => props.theme.color.fontColorWhite};
+  font-size: 2rem;
+  margin: 1rem 0;
+`;
 
 const PageNotFound = () => {
-  return <div>페이지를 찾을 수 없습니다.</div>;
+  const navigate = useNavigate();
+  return (
+    <Wrapper>
+      <p>페이지를 찾을 수 없습니다.</p>
+      <ConfirmButton onClick={() => navigate("/")}>
+        메인으로 이동하기
+      </ConfirmButton>
+    </Wrapper>
+  );
 };
 
 export default PageNotFound;
