58f36030c2eed6dc86e69032c0e351f516f07f45
movie42 <movie3342@gmail.com>
2022-11-22 23:32:17 +0900

    chord: prettierignore 추가 scripts 파일 삭제

--- scripts/after-fedeploy.sh
+++
@@ -1,6 +0,0 @@
-# #!/bin/bash
-
-# sudo cp -rf /home/ubuntu/deploy/client/* /home/ubuntu/client/ && rm -rf /home/ubuntu/deploy/client/
-# sudo service nginx reload
-# x
-# echo "> Nginx reloaded"
--- scripts/before-fedeploy.sh
+++
@@ -1,6 +0,0 @@
-# #!/bin/bash
-
-# mkdir -v /home/ubuntu/deploy/client/
-# mkdir -v /home/ubuntu/deploy/server/
-
-# echo "> location file delete and make folder"
