3046dec6f08deaac948101cc67d1544880861ba4
sycho09 <wyswid@naver.com>
2022-11-24 11:13:58 +0900

    style: 컴포넌트 별 스타일 분리

--- src/Page/Client/ClientMenu/styles.ts
+++ src/Page/Client/ClientMenu/styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import { Headline1 } from "../../../lib/styles/mixin";
+import { ButtonDefaultStyle } from "../../../Components";
+import { Headline1, SubTitle2 } from "../../../lib/styles/mixin";
 
 export const Header = styled.div`
   display: flex;
@@ -88,4 +89,91 @@ export const Item = styled.li`
       }
     }
   }
+`;
+
+// Modal style
+export const Wrapper = styled.div`
+  position: relative;
+  color: ${(props) => props.theme.color.fontColorBlack};
+  .image-container {
+    overflow: hidden;
+    position: relative;
+    height: 25rem;
+    .transparent-box {
+      position: absolute;
+      z-index: 1;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 100%;
+      background-color: ${({ theme }) => theme.color.backgroundBlack60};
+    }
+  }
+  .item-info-container {
+    align-self: center;
+    padding: 2rem;
+    .item-title {
+      ${SubTitle2}
+    }
+    h4 {
+      font-size: 2rem;
+      align-self: end;
+    }
+  }
+`;
+
+export const ItemNameContainer = styled.div`
+  display: flex;
+  font-size: 1.4rem;
+  font-weight: 600;
+  padding: 0.5rem;
+  align-items: center;
+  color: ${({ theme }) => theme.color.fontColorWhite};
+  margin: 0.4rem;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+  h2 {
+    margin-left: 0.5rem;
+  }
+`;
+export const OptionButton = styled(ButtonDefaultStyle)<{ selected?: boolean }>`
+  background-color: ${(props) =>
+    props.selected ? props.theme.color.primary800 : props.theme.color.gray300};
+  margin-right: 1rem;
+  font-size: 1.6rem;
+  flex-grow: 1;
+`;
+export const OrderContainer = styled.div`
+  display: grid;
+  grid-template-columns: 50px 0.5fr 50px 1fr 1fr;
+  column-gap: 10px;
+  align-items: center;
+`;
+export const OptionContainer = styled.div`
+  display: flex;
+`;
+export const CancelButton = styled(ButtonDefaultStyle)`
+  background-color: ${(props) => props.theme.color.gray300};
+  padding: 0.8rem;
+  font-family: "Noto Sans KR";
+`;
+export const OrderButton = styled(ButtonDefaultStyle)`
+  background-color: ${(props) => props.theme.color.primary800};
+  padding: 0.8rem;
+  font-family: "Noto Sans KR";
+`;
+export const Title = styled.h4<{ composition?: boolean }>`
+  font-size: 1.7rem;
+  font-weight: bold;
+  margin: ${(props) =>
+    props.composition ? "0.5rem 0 0.5rem" : "2rem 0 0.5rem"};
+`;
+export const ProductCount = styled.span`
+  font-size: 2rem;
+  font-weight: bold;
+  margin: 0 1.5rem 0.8rem;
+  text-align: center;
 `;
--- src/Page/Client/ClientSelectList/styles.ts
+++ src/Page/Client/ClientSelectList/styles.ts
@@ -3,6 +3,7 @@ import { ButtonDefaultStyle } from "../../../Components";
 import {
   Body1,
   Headline1,
+  Headline2,
   SubTitle1,
   SubTitle2
 } from "../../../lib/styles/mixin";
@@ -32,7 +33,7 @@ export const Header = styled.div`
 export const MenuListWrapper = styled.div`
   margin: 1rem 0;
 `;
-export const MenuListItem = styled.div`
+export const MenuListItemBox = styled.div`
   display: grid;
   grid-template-columns: 20% 50% 30%;
   padding: 0.5rem 0;
@@ -85,4 +86,51 @@ export const MinusCountButton = styled(ButtonDefaultStyle)`
   color: ${(props) => props.theme.color.error500};
   font-size: 2.3rem;
   vertical-align: middle;
+`;
+
+// Modal
+export const PaymentBox = styled.div`
+  h1 {
+    text-align: center;
+  }
+  h2 {
+    ${Headline2};
+  }
+  h3 {
+    ${SubTitle1};
+  }
+  h4 {
+    ${SubTitle2};
+    font-weight: 700;
+  }
+  span {
+    ${Body1};
+  }
+  p {
+    ${Body1};
+    font-weight: 700;
+  }
+`;
+export const MenuBox = styled.div`
+  display: grid;
+  grid-template-columns: 3fr 1fr 1fr;
+  padding-bottom: 0.5rem;
+  margin-bottom: 1rem;
+  border-bottom: 1px solid black;
+  text-align: right;
+  span:nth-child(3n-2) {
+    text-align: left;
+  }
+`;
+export const BtnGroup = styled.div`
+  text-align: center;
+  margin-top: 1rem;
+`;
+export const ConfirmButton = styled(ButtonDefaultStyle)`
+  background-color: ${(props) => props.theme.color.primary500};
+  margin: 0 0.5rem;
+`;
+export const CancelButton = styled(ButtonDefaultStyle)`
+  background-color: ${(props) => props.theme.color.gray300};
+  margin: 0 0.5rem;
 `;
