ec6b1e6189ede64e95fad0151237db4dec793bc1
A <movie3342@gmail.com>
2022-11-27 21:01:14 +0900

    refactor: ProductItem 스타일드 컴포넌트 분리

--- src/Page/Admin/Product/AdminManageProductItem/ProductItem.tsx
+++ src/Page/Admin/Product/AdminManageProductItem/ProductItem.tsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { motion, Variants } from "framer-motion";
+import { motion } from "framer-motion";
 import { MdCreate } from "react-icons/md";
 import { useQueryClient } from "react-query";
 import { useNavigate, useParams } from "react-router-dom";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import styled from "styled-components";
 
 import {
   Modal,
   ProductUpdateModal,
-  ButtonDefault,
   ToggleButton,
   Images,
   Noimage
@@ -18,151 +16,28 @@ import { useToggleProductIsAvailableMutation } from "@/lib/generated/graphql";
 import graphqlReqeustClient from "@/lib/graphqlRequestClient";
 import {
   ProductListValues,
-  Option,
   selectOptionState,
   selectProductListState,
   updateProductState,
   userState
 } from "@/lib/state";
-
-import useModalHook from "@/lib/hooks/useModalHook";
+import { useModalHook } from "@/lib/hooks";
 import { translateLocalCurrency } from "@/lib/utils";
-
-const ItemWrapper = styled(motion.li)`
-  position: relative;
-`;
-
-const Item = styled(motion.div)<{ selectOption: Option; selected: boolean }>`
-  .item-container {
-    position: relative;
-    cursor: pointer;
-    background-color: ${(props) => props.theme.color.background100};
-    height: 100%;
-    min-width: 20rem;
-    overflow: hidden;
-    display: grid;
-    grid-template-rows: minmax(12rem, 0.9fr) 0.7fr;
-    border: 1px solid ${({ theme }) => theme.color.gray300};
-    border-radius: 0.4rem;
-    text-decoration: none;
-    color: ${(props) => props.theme.color.fontColorBlack};
-    .is-select {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-color: ${(props) =>
-        props.selected ? "unset" : props.theme.color.background80};
-      z-index: 11;
-    }
-    .image-container {
-      overflow: hidden;
-      position: relative;
-      .transparent-box {
-        position: absolute;
-        z-index: 1;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        height: 100%;
-        background-color: ${({ theme }) => theme.color.backgroundBlack60};
-      }
-    }
-    .item-info-container {
-      align-self: center;
-      padding: 0.8rem;
-      ${({ theme }) => theme.device.tablet} {
-        padding: 2rem;
-      }
-      h3 {
-        font-size: 3rem;
-        font-weight: bold;
-        margin-bottom: 0.6rem;
-      }
-      h4 {
-        font-size: 2rem;
-        align-self: end;
-      }
-    }
-  }
-`;
-const ButtonContainer = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 11;
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const ToggleContainer = styled.div`
-  display: flex;
-  font-size: 1.4rem;
-  font-weight: 600;
-  padding: 0.5rem;
-  align-items: center;
-  color: ${({ theme }) => theme.color.fontColorWhite};
-  margin: 0.4rem;
-  p {
-    margin-left: 0.5rem;
-    ${({ theme }) => theme.device.tablet} {
-      font-size: 2.8rem;
-    }
-  }
-`;
-
-const UpdateButtonWrapper = styled.div`
-  padding-right: 1rem;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  color: ${({ theme }) => theme.color.fontColorWhite};
-  font-size: 2.2rem;
-  ${({ theme }) => theme.device.tablet} {
-    font-size: 2.8rem;
-  }
-
-  button {
-    background-color: unset;
-    padding: 0;
-    padding-right: 0.5rem;
-    ${({ theme }) => theme.device.tablet} {
-      font-size: 2.8rem;
-    }
-  }
-`;
-
-const UpdateProductButton = styled(ButtonDefault)`
-  font-size: 1.5rem;
-`;
+import {
+  boxVariants,
+  imageBoxVariants,
+  Item,
+  ItemWrapper,
+  ProductItemButtonContainer,
+  ToggleContainer,
+  UpdateButtonWrapper,
+  UpdateProductButton
+} from "./styles";
 
 interface IProductItemProps extends React.HTMLAttributes<HTMLLIElement> {
   productData?: ProductListValues[];
 }
 
-const boxVariants: Variants = {
-  init: { scale: 1 },
-  hover: {
-    scale: 1,
-    transition: {
-      duration: 0.4
-    }
-  }
-};
-
-const imageBoxVariants: Variants = {
-  init: { scale: 1 },
-  hover: {
-    scale: 2,
-    transition: {
-      duration: 0.4
-    }
-  }
-};
 const ProductItem = ({ productData }: IProductItemProps) => {
   const navigate = useNavigate();
   const { userId, storeId } = useParams();
@@ -234,7 +109,7 @@ const ProductItem = ({ productData }: IProductItemProps) => {
           whileHover="hover"
           key={product.id}
         >
-          <ButtonContainer>
+          <ProductItemButtonContainer>
             <ToggleContainer>
               <ToggleButton
                 onClick={() => toggleProductValue({ id: product.id })}
@@ -247,7 +122,7 @@ const ProductItem = ({ productData }: IProductItemProps) => {
               <MdCreate />
               <UpdateProductButton>수정</UpdateProductButton>
             </UpdateButtonWrapper>
-          </ButtonContainer>
+          </ProductItemButtonContainer>
           <Item
             key={product.id}
             data-id={product.id}
--- src/Page/Admin/Product/AdminManageProductItem/styles.ts
+++ src/Page/Admin/Product/AdminManageProductItem/styles.ts
@@ -1,5 +1,6 @@
 import { ButtonDefault } from "@/Components";
 import { Option } from "@/lib/state";
+import { motion, Variants } from "framer-motion";
 import styled, { css } from "styled-components";
 
 export const Container = styled.div``;
@@ -61,3 +62,138 @@ export const ButtonItemWrapper = styled.div`
 
 export const CreateProductButton = styled(ButtonDefault)``;
 export const DeleteProductButton = styled(ButtonDefault)``;
+
+export const ItemWrapper = styled(motion.li)`
+  position: relative;
+`;
+
+export const Item = styled(motion.div)<{
+  selectOption: Option;
+  selected: boolean;
+}>`
+  .item-container {
+    position: relative;
+    cursor: pointer;
+    background-color: ${(props) => props.theme.color.background100};
+    height: 100%;
+    min-width: 20rem;
+    overflow: hidden;
+    display: grid;
+    grid-template-rows: minmax(12rem, 0.9fr) 0.7fr;
+    border: 1px solid ${({ theme }) => theme.color.gray300};
+    border-radius: 0.4rem;
+    text-decoration: none;
+    color: ${(props) => props.theme.color.fontColorBlack};
+    .is-select {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: ${(props) =>
+        props.selected ? "unset" : props.theme.color.background80};
+      z-index: 11;
+    }
+    .image-container {
+      overflow: hidden;
+      position: relative;
+      .transparent-box {
+        position: absolute;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+        background-color: ${({ theme }) => theme.color.backgroundBlack60};
+      }
+    }
+    .item-info-container {
+      align-self: center;
+      padding: 0.8rem;
+      ${({ theme }) => theme.device.tablet} {
+        padding: 2rem;
+      }
+      h3 {
+        font-size: 3rem;
+        font-weight: bold;
+        margin-bottom: 0.6rem;
+      }
+      h4 {
+        font-size: 2rem;
+        align-self: end;
+      }
+    }
+  }
+`;
+export const ProductItemButtonContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 11;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ToggleContainer = styled.div`
+  display: flex;
+  font-size: 1.4rem;
+  font-weight: 600;
+  padding: 0.5rem;
+  align-items: center;
+  color: ${({ theme }) => theme.color.fontColorWhite};
+  margin: 0.4rem;
+  p {
+    margin-left: 0.5rem;
+    ${({ theme }) => theme.device.tablet} {
+      font-size: 2.8rem;
+    }
+  }
+`;
+
+export const UpdateButtonWrapper = styled.div`
+  padding-right: 1rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.color.fontColorWhite};
+  font-size: 2.2rem;
+  ${({ theme }) => theme.device.tablet} {
+    font-size: 2.8rem;
+  }
+
+  button {
+    background-color: unset;
+    padding: 0;
+    padding-right: 0.5rem;
+    ${({ theme }) => theme.device.tablet} {
+      font-size: 2.8rem;
+    }
+  }
+`;
+
+export const UpdateProductButton = styled(ButtonDefault)`
+  font-size: 1.5rem;
+`;
+
+export const boxVariants: Variants = {
+  init: { scale: 1 },
+  hover: {
+    scale: 1,
+    transition: {
+      duration: 0.4
+    }
+  }
+};
+
+export const imageBoxVariants: Variants = {
+  init: { scale: 1 },
+  hover: {
+    scale: 2,
+    transition: {
+      duration: 0.4
+    }
+  }
+};
