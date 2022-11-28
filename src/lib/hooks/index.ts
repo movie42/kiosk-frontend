2ca2345384c64f917553df82cd3fb9be98284896
A <movie3342@gmail.com>
2022-11-28 12:09:11 +0900

    feat: LocalStorageService 구현
    useGetUserStateFromLocalStorage, useRemoveUserInfoInLocalStorage, useGetUserStateFromLocalStorage
    로컬 스토리지를 세팅하는 이 훅은 내부 구현이 커스텀 훅이라고 할 수 없기 때문에 삭제하였습니다.
    LocalStorageService
    - getLocalStorage 로컬 스토리지에서 사용자 상태를 가져옵니다.
    - setLocalStorage 로컬 스토리지에 사용자 상태를 세팅합니다.
    - removeLocalStorage 로컬 스토리지에서 사용자 상태를 제거합니다.

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -1,6 +1,3 @@
-export { default as useGetUserStateFromLocalStorage } from "./useGetUserStateFromLocalStorage";
 export { default as useImageUpload } from "./useImageUpload";
 export { default as useModalHook } from "./useModalHook";
 export { default as usePost } from "./usePost";
-export { default as useRemoveUserInfoInLocalStorage } from "./useRemoveUserInfoInLocalStorage";
-export { default as useSetUserInfoToLocalStorage } from "./useSetUserInfoToLocalStorage";
