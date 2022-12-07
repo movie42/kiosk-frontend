824c78da6b93b369b8bd02717b600a3974d35851
sycho09 <wyswid@naver.com>
2022-12-07 18:22:12 +0900

    style: order state list 스타일 파일 분리

--- src/Components/UI/Organisms/Containers/styles.ts
+++ src/Components/UI/Organisms/Containers/styles.ts
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { motion } from "framer-motion";
+import styled, { DefaultTheme } from "styled-components";
+import { motion, Variants } from "framer-motion";
 import { Headline2, Headline3 } from "@/lib/styles/mixin";
 import { InputDefault } from "@/Components/UI/Atoms";
 
@@ -81,4 +81,156 @@ export const ButtonContainer = styled(motion.div)`
       margin-left: 0;
     }
   }
+`;
+
+export const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(20rem, 40rem);
+  gap: 2rem;
+  ${({ theme }) => theme.device.tablet} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  ${({ theme }) => theme.device.mobile} {
+    grid-template-columns: unset;
+  }
+
+  li {
+    box-sizing: border-box;
+    overflow-y: auto;
+    &.list-container {
+      align-items: flex-start;
+      margin-bottom: 0.8rem;
+      border: 1px solid ${(props) => props.theme.color.gray300};
+      border-radius: 1rem;
+      .list-header {
+        display: flex;
+        justify-content: space-between;
+        padding-bottom: 1rem;
+        span {
+          ${Headline2}
+          line-height: 1.2;
+          &:not(:first-child) {
+            margin-left: 1rem;
+            font-size: 1.7rem;
+          }
+          font-weight: 900;
+          strong {
+            display: block;
+            font-size: 1.7rem;
+            font-weight: 300;
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const OrderProductInfoContainer = styled.div`
+  ul {
+    li {
+      display: grid;
+      padding: 1rem 0.8rem;
+      &:not(:last-child) {
+        border-bottom: 2px dotted ${(props) => props.theme.color.gray300};
+      }
+      span {
+        &:first-child {
+          grid-column: 1 / span 3;
+          padding-bottom: 0.8rem;
+        }
+        &:nth-child(2) {
+          grid-column: 1 / 2;
+        }
+        &:nth-child(3) {
+          grid-column: 2 / 3;
+        }
+        &:last-child {
+          grid-column: 3 / 4;
+        }
+        font-size: 1.7rem;
+        line-height: 1.3;
+        font-weight: 600;
+        strong {
+          display: block;
+          font-size: 1.3rem;
+          font-weight: 300;
+        }
+      }
+    }
+  }
+`;
+
+export const OrderInfoHeader = styled(motion.div)`
+  position: sticky;
+  height: 15.5rem;
+  top: 0;
+  box-sizing: border-box;
+  z-index: 1;
+  background-color: ${({ theme }) => theme.color.gray800};
+  padding: 1rem;
+  color: ${({ theme }) => theme.color.fontColorWhite};
+  .order-button-container {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+export const buttonBlinkVariants: Variants = {
+  animation: {
+    backgroundColor: ["#ffe100", "#d0d0d0"],
+    transition: {
+      backgroundColor: { yoyo: Infinity, duration: 0.8 }
+    }
+  }
+};
+
+export const defaultButtonStyle = styled(motion.button)<{ status: string }>`
+  cursor: pointer;
+  font-size: 1.2rem;
+  padding: 0 0.8rem;
+  margin-bottom: 0.3rem;
+  width: 5rem;
+  height: 5rem;
+  border-radius: 100%;
+  border: 2px solid #fff;
+`;
+
+export const buttonFalse = (theme: DefaultTheme) => `
+    background-color: ${theme.color.gray300};
+    color:${theme.color.fontColorBlack};
+    border:1px solid #000;
+`;
+
+export const ReadyOrderButton = styled(defaultButtonStyle)`
+  ${({ status, theme }) =>
+    status === "READY"
+      ? `background-color: ${theme.color.secondary300};
+         color:${theme.color.fontColorWhite};
+            `
+      : `${buttonFalse(theme)}`};
+`;
+export const DoneOrderButton = styled(defaultButtonStyle)`
+  ${({ status, theme }) =>
+    status === "DONE"
+      ? `background-color: ${theme.color.secondary600};
+         color:${theme.color.fontColorWhite};
+            `
+      : `${buttonFalse(theme)}`};
+`;
+export const CompleteOrderButton = styled(defaultButtonStyle)`
+  ${({ status, theme }) =>
+    status === "COMPLETE"
+      ? `background-color: ${theme.color.primary500};
+         color:${theme.color.fontColorWhite};
+        `
+      : `${buttonFalse(theme)}`};
+`;
+export const CancelOrderButton = styled(defaultButtonStyle)`
+  ${({ status, theme }) =>
+    status === "CANCELED"
+      ? `background-color: ${theme.color.error700};
+         color:${theme.color.fontColorWhite};
+            `
+      : `${buttonFalse(theme)}`};
 `;
