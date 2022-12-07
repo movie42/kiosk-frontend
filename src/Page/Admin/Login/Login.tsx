3f711ff6b2d280107885a15f686a86cfd128afcc
A <movie3342@gmail.com>
2022-12-07 15:02:25 +0900

    refactor: useLogin, Login 리펙토링
      Login
        - Form 컴포넌트 적용
      useLogin
        - useFormContext를 사용합니다.
        - FormProvider 안에서만 사용됩니다.

--- src/Page/Admin/Login/Login.tsx
+++ src/Page/Admin/Login/Login.tsx
@@ -1,73 +1,26 @@
-import { Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
 import { useRecoilValue } from "recoil";
 
-import { LabelDefault } from "@/Components/UI/Atoms";
+import { Form } from "@/Components/UI/Atoms";
 import { AdminLoadingAndGetUser } from "@/Components/UI/Molecules";
 import { PageHeader } from "@/Components/UI/Organisms";
 import { EMAIL_REX } from "@/lib/constant/constant";
 import { userState } from "@/lib/state/userState";
 import { useLogin } from "../hooks";
+import { FormContainer, Wrapper } from "./styles";
 import { IUserProps } from "../interface";
-import {
-  ActionContainer,
-  FormContainer,
-  LoginInput,
-  LoginLabel,
-  Wrapper
-} from "./styles";
 
 const Login = () => {
-  const { mutate } = useLogin();
+  const method = useForm<IUserProps>();
   const isUser = useRecoilValue(userState);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm<IUserProps>();
-
-  const onSubmit = handleSubmit((data: IUserProps) => {
-    mutate({ ...data });
-  });
 
   return !isUser.isLogin ? (
     <Wrapper>
       <PageHeader header="로그인" />
       <FormContainer>
-        <form onSubmit={onSubmit}>
-          <LoginLabel htmlFor="email">이메일</LoginLabel>
-          <LoginInput
-            id="email"
-            type="email"
-            placeholder="이메일을 입력해주세요."
-            {...register("email", {
-              required: "이메일을 입력하세요",
-              pattern: {
-                value: EMAIL_REX,
-                message: "이메일이 아닙니다."
-              }
-            })}
-          />
-          <LoginLabel htmlFor="password">비밀번호</LoginLabel>
-          <LoginInput
-            id="password"
-            type="password"
-            placeholder="비밀번호를 입력해주세요."
-            {...register("password", {
-              required: "비밀번호를 입력해주세요."
-            })}
-          />
-          {errors?.loginFail?.message && (
-            <LabelDefault className="error-label">
-              {errors?.loginFail?.message}
-            </LabelDefault>
-          )}
-          <ActionContainer>
-            <Link to="#">아이디 또는 비밀번호를 잃어버리셨나요?</Link>
-            <button>로그인</button>
-          </ActionContainer>
-        </form>
+        <FormProvider {...method}>
+          <LoginFormContainer />
+        </FormProvider>
       </FormContainer>
     </Wrapper>
   ) : (
@@ -76,3 +29,54 @@ const Login = () => {
 };
 
 export default Login;
+
+const LoginFormContainer = () => {
+  const { mutate } = useLogin();
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors }
+  } = useFormContext<IUserProps>();
+
+  const handleLoginForm = handleSubmit((data: IUserProps) => {
+    mutate(data);
+  });
+
+  return (
+    <Form onSubmit={handleLoginForm}>
+      <Form.FormItemContainer>
+        <Form.Label htmlFor="email">이메일</Form.Label>
+        <Form.Input
+          id="email"
+          type="email"
+          placeholder="이메일을 입력해주세요."
+          {...register("email", {
+            required: "이메일을 입력하세요",
+            pattern: {
+              value: EMAIL_REX,
+              message: "이메일이 아닙니다."
+            }
+          })}
+        />
+      </Form.FormItemContainer>
+      {errors.email && <Form.Label>{errors.email.message}</Form.Label>}
+      <Form.FormItemContainer>
+        <Form.Label htmlFor="password">비밀번호</Form.Label>
+        <Form.Input
+          id="password"
+          type="password"
+          placeholder="비밀번호를 입력해주세요."
+          {...register("password", {
+            required: "비밀번호를 입력해주세요."
+          })}
+        />
+      </Form.FormItemContainer>
+      {errors.password && <Form.Label>{errors.password.message}</Form.Label>}
+      {errors.loginFail ? (
+        <Form.Label>{errors.loginFail.message}</Form.Label>
+      ) : null}
+      <Form.Button>로그인</Form.Button>
+    </Form>
+  );
+};
