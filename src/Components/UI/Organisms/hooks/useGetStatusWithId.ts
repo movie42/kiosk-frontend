0eaebfbc882f5cb6fd37bffcab380d01dd75ee94
sycho09 <wyswid@naver.com>
2022-12-10 03:25:45 +0900

    refactor: 연관있는 데이터를 기준으로 커스텀 훅 리팩토링(선택한 주문 id와 상태 저장, 주문 상태 변경 mutation 으로 분리)

---
+++ src/Components/UI/Organisms/hooks/useGetStatusWithId.ts
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { OrderStatusValue } from "@/lib/state/interface";
+
+export const useGetStatusWithId = () => {
+  const [orderId, setOrderId] = useState("");
+  const [orderStatus, setOrderStatus] = useState<OrderStatusValue | null>(null);
+
+  const getStatusWithId = (id: string, status: OrderStatusValue | null) => {
+    setOrderId(() => id);
+    setOrderStatus(() => status);
+  };
+
+  return { getStatusWithId, orderId, orderStatus };
+};
