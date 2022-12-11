e7745db9e906db0aceee7424d870e9d7ac2c1833
sycho09 <wyswid@naver.com>
2022-12-11 17:11:03 +0900

    style: molecules 폴더 내 컴포넌트 스타일 분리

--- src/Components/UI/Molecules/Bar/styles.ts
+++ src/Components/UI/Molecules/Bar/styles.ts
@@ -1,5 +1,6 @@
-import { SubTitle1 } from "@/lib/styles";
+import { SubTitle1, SubTitle2 } from "@/lib/styles";
 import styled from "styled-components";
+import { BasicSquareButton } from "../../Atoms";
 
 export const StatusBar = styled.div`
   position: fixed;
@@ -48,4 +49,44 @@ export const StatusBar = styled.div`
       justify-content: space-around;
     }
   }
+`;
+
+export const MenuBarContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 2rem;
+
+  background-color: ${(props) => props.theme.color.background100};
+  color: ${(props) => props.theme.color.fontColorBlack};
+  height: 6rem;
+  z-index: 20;
+  border: 1px solid ${(props) => props.theme.color.gray300};
+  h2 {
+    ${SubTitle2}
+  }
+
+  button {
+    cursor: pointer;
+    border: 0;
+    font-size: 2rem;
+    color: ${(props) => props.theme.color.fontColorWhite};
+    border-radius: 0.3rem;
+    line-height: 2.8rem;
+  }
+`;
+
+export const ConfirmButton = styled(BasicSquareButton)`
+  background-color: ${(props) => props.theme.color.error500};
+`;
+
+export const CancelButton = styled(BasicSquareButton)`
+  background-color: ${(props) => props.theme.color.gray400};
+  color: ${(props) => props.theme.color.fontColorWhite};
+  margin-right: 0.3rem;
 `;
