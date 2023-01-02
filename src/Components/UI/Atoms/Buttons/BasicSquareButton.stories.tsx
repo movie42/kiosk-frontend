fbe7166fa8ba0979913b9115a34b3f76c3d00f22
movie42 <movie3342@gmail.com>
2023-01-02 14:17:19 +0900

    chord: 스토리북 설치 및 테스트 환경 세팅

---
+++ src/Components/UI/Atoms/Buttons/BasicSquareButton.stories.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { BasicSquareButton } from ".";
+
+export default {
+  title: "BasicSquareButton",
+  component: BasicSquareButton
+};
+
+export const BasicSquareButtonTest = () => (
+  <BasicSquareButton>안녕</BasicSquareButton>
+);
