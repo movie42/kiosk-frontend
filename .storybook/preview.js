8ccaa1e9df3de8d07c85feb14cd5fb345eb9d28f
movie42 <movie3342@gmail.com>
2023-01-08 12:02:06 +0900

    chord: 스토리북 에드온 actions, a11y 설치

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "../src/lib/styles/theme";
+import { withA11y } from "@storybook/addon-a11y";
+import { addDecorator } from "@storybook/react";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,8 +10,16 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/
     }
+  },
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
   }
 };
+
+addDecorator(withA11y);
 
 export const decorators = [
   (Story) => {
